98ea614ad25c8e769ce650a7d9786afa022011a9
AbhiMejo <abhishek.mejo@gmail.com>
2024-02-13 17:58:17 +0530

    good job

--- hi.java
+++ hi.java
@@ -1,5 +1,9 @@
+import java.util.Date;
+
 class hi{
     public static void main(String[] args) {
         System.out.println("Hi Deeksha");
+        System.out.println(new Date());
+        System.err.println("Great Job");
     }
 }
